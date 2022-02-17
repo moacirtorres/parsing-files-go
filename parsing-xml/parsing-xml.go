d2530ab2a430508b150c38f5bee3f1830600bf9b
moacirtorres <moacirtorress@gmail.com>
2022-02-16 22:30:56 -0300

    ✨ converting xml logic

---
+++ parsing-xml/parsing-xml.go
@@ -0,0 +1,58 @@
+package parsingXml
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// our struct which contains the complete
+// array of all Users in the file
+type Users struct {
+	XMLName xml.Name `xml:"users"`
+	Users   []User   `xml:"user"`
+}
+
+// the user struct, this contains our
+// Type attribute, our user's name and
+// a social struct which will contain all
+// our social links
+type User struct {
+	XMLName xml.Name `xml:"user"`
+	Type    string   `xml:"type,attr"`
+	Name    string   `xml:"name"`
+	Social  Social   `xml:"social"`
+}
+
+// a simple struct which contains all our
+// social links
+type Social struct {
+	XMLName  xml.Name `xml:"social"`
+	Facebook string   `xml:"facebook"`
+	Twitter  string   `xml:"twitter"`
+	Youtube  string   `xml:"youtube"`
+}
+
+func ReadXML() {
+
+	// Open our xmlFile
+	xmlFile, err := os.Open("example.xml")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened users.xml")
+	// defer the closing of our xmlFile so that we can parse it later on
+	defer xmlFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+
+	var users Users
+
+	xml.Unmarshal([]byte(byteValue), &users)
+
+	fmt.Println(users)
+
+}
